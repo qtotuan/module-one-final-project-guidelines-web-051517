38a6d6b10c1ed1de5330effbab06459661a9bc92
qtotuan <quynh.totuan@gmail.com>
2017-05-31 09:16:22 -0400

    Finished seeding.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,17 @@ csv_array = CSV.read("incidents.csv")
 # end
 
 def parse_date(string)
+  return nil if string == nil || string.empty?
   year = string.split(" ")[0].split("/")[2].to_i
   month = string.split(" ")[0].split("/")[0].to_i
   day = string.split(" ")[0].split("/")[1].to_i
-  date = DateTime.new(year, month, day)
+  date = Date.new(year, month, day)
 end
 
-csv_array.each do |row_array|
+csv_array.each_with_index do |row_array, index|
+  next if index == 0 # ignore headers
   borough = Borough.find_by_name(row_array[2])
   incidenttype = Incidenttype.find_by_name(row_array[0])
+  # binding.pry
   Incidenttype_Borough.create(borough: borough, incidenttype: incidenttype, open_date: parse_date(row_array[3]), close_date: parse_date(row_array[4]))
 end
