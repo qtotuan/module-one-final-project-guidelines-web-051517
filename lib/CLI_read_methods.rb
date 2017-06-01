046ae3c08bf916772dce9609b4796716aa6e30d3
zluo16 <zluo@alumni.berklee.edu>
2017-06-01 09:37:42 -0400

    in the middle of dealing with non-options

--- lib/CLI_read_methods.rb
+++ lib/CLI_read_methods.rb
@@ -3,10 +3,18 @@ require "pry"
 def filter_by_borough
   puts "Which borough would you like to view?"
   borough = gets.chomp
-  b = Borough.find_by_name(borough)
-  incidents = Incidenttype_Borough.where(borough: b)
-  # binding.pry
-  incidents.each {|i| puts "#{i.incidenttype.name} - #{i.open_date}"}
+
+  if Borough.find_by_name(borough)
+    b = Borough.find_by_name(borough)
+    incidents = Incidenttype_Borough.where(borough: b)
+    # binding.pry
+    incidents.each {|i| puts "#{i.incidenttype.name} - #{i.open_date}"}
+    puts "Sorry, that's not a valid entry, please type "
+  elsif borough == "exit" || borough == "return" || borough == "menu"
+    
+  else
+  end
+
 end
 
 # def filter_by_date_range
